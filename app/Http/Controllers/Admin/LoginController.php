39d3c4b069ebada99c631652574fc2ba8a8196e5
alan <1993826783@qq.com>
2018-06-04 10:21:22 +0800

    Initial commit

---
+++ app/Http/Controllers/Admin/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Model\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Input;
+
+require_once 'org/code/Code.class.php';
+
+class LoginController extends CommonController
+{
+    public function index()
+    {
+        return view('admin.login');
+    }
+
+    public function code()
+    {
+        $code = new \Code();
+        $code->make();
+    }
+
+
+    public function login()
+    {
+        if($input = Input::all()){
+            $code = new \Code();
+            $_code = $code->get();
+            if($_code != strtoupper($input['code']))
+                return back()->with('msg', '验证码错误！');
+            $user = User::first();
+            if($user->name != $input['username'] || Crypt::decrypt($user->password) != $input['password'])
+                return back()->with('msg', '用户名或密码错误！');
+            session(['user'=>$user]);
+            return redirect('admin/index');
+        }
+        else{
+            return view('admin.login');
+        }
+    }
+
+    public function quit()
+    {
+        session(['user'=>null]);
+        return redirect('admin/login');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::group(['middleware'=>'web', 'prefix'=>'admin', 'namespace'=>'Admin'], function () {
+    Route::any('login', 'LoginController@login');
+    Route::get('code', 'LoginController@code');
+});
+
+
+Route::group(['middleware' => ['web', 'admin.login'],'prefix'=>'admin', 'namespace'=>'Admin' ], function () {
+
+    Route::get('index', 'IndexController@index');
+    Route::get('quit', 'LoginController@quit');
+});
+
