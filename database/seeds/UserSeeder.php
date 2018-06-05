590e8199f95fb6852e0ebb870da00533eb148700
Alan <liuqingfeng@bupt.edu.cn>
2018-06-05 18:03:09 +0800

    下一步尝试用migrate创建教程中的数据库（文章分类表），并用seed填充

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \Illuminate\Support\Facades\DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => \Illuminate\Support\Facades\Crypt::encrypt('admin')
+        ]);
+    }
+}
